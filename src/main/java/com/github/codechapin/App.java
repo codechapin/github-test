63d2e717c5d22e181ffdfe5dabc70098bb71ae09
Juan Carlos Aguilar <codechapin@gmail.com>
2019-02-06 19:00:51 -0800

    Initial gradle setup.

---
+++ src/main/java/com/github/codechapin/App.java
@@ -0,0 +1,11 @@
+package com.github.codechapin;
+
+
+import static com.github.codechapin.utils.IO.println;
+
+public class App {
+
+  public static void main(String[] args) {
+    println("hello world");
+  }
+}
---
+++ src/main/java/com/github/codechapin/utils/IO.java
@@ -0,0 +1,8 @@
+package com.github.codechapin.utils;
+
+public class IO {
+
+  public static void println(String out) {
+    System.out.println(out);
+  }
+}
